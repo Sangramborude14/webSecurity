a116a82290f42d80517c157f043012112acaba48
Sangram <sangram2mail@gmail.com>
2026-07-25 18:36:37 +0530

    attacker setup (CSRF)

---
+++ CSRF/attacker-server.js
@@ -0,0 +1,13 @@
+// Step 1: Create your Attacker Server here! (Port 5000)
+const express = require('express');
+const path = require('path');
+const server = express();
+
+server.get('/win', (req,res) => {
+    res.sendFile(path.join(__dirname,'attacker.html'));
+});
+
+const PORT = 5000;
+server.listen(PORT,() => {
+    console.log(`attacker server http://localhost:5000`)
+})

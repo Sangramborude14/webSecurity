f2d87cd60e28986f574f40fcb1dbc4e2e2a9d11d
Sangram <sangram2mail@gmail.com>
2026-07-25 16:35:38 +0530

    credentials CORS error

--- CORS/backend.js
+++ CORS/backend.js
@@ -10,6 +10,12 @@ app.get(`/api/data`, (req,res) => {
     res.json({message: `Hello from backend`})
 })
 
+app.post(`/api/credentials`, (req,res) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Credentials',true);
+    res.json({message: 'Credential Endpoint Called'});
+})
+
 app.options('/api/preflight',(req,res) => {
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,OPTIONS');

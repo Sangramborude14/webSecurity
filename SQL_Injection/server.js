db50afc21a3bee7f9e3932748868cc8155bced2d
Sangram <sangram2mail@gmail.com>
2026-07-31 16:25:44 +0530

    SQL: fixed vulnerable authentication server

--- SQL_Injection/server.js
+++ SQL_Injection/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Pool } = require("pg");
+const bcrypt = require('bcrypt');
 require("dotenv").config();
 
 const app = express();
@@ -8,35 +9,56 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
+
+
+
+
 // Vulnerable login endpoint
 app.get("/login", async (req, res) => {
     const username = req.query.username;
     const password = req.query.password;
 
     const query = `
-        SELECT id, username
+        SELECT id, username,password
         FROM users
-        WHERE username = '${username}'
-        AND password = '${password}'
+        WHERE username = $1
     `;
 
     console.log("Executing:", query);
 
     try {
-        const result = await pool.query(query);
+        const result = await pool.query(query,[username]);
 
-        if (result.rows.length > 0) {
-            return res.json({
-                success: true,
-                message: "Login successful",
-                user: result.rows[0]
-            });
-        }
-
-        res.status(401).json({
+        if(result.rows.length === 0){
+            res.status(401).json({
             success: false,
             message: "Invalid username or password"
         });
+        }
+
+        const user = result.rows[0];
+        
+        const passwordCorrect = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if(!passwordCorrect){
+            return res.status(401).json({
+                message: "Invalid username or Password"
+            });
+        }
+
+        res.json({
+            success: true,
+            message: "Login successfull",
+            user: {
+                id: user.id,
+                username: user.username
+            }
+        })
+
+        
 
     } catch (error) {
         console.error(error);

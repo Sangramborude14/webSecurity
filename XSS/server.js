eff785683dd4f6b0c089b82591f719582037b00a
Sangram <sangram2mail@gmail.com>
2026-07-31 08:08:07 +0530

    XSS: Fixed vulnerability for Reflected XSS

--- XSS/server.js
+++ XSS/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = requrie('path');
+const path = require('path');
 
 const app = express();
 
@@ -17,9 +17,41 @@ app.get("/comments",(req,res) => {
     res.json(comments);
 })
 
+app.get("/search",(req,res) => {
+    const query = req.query.q || "";
+
+    res.send(`
+        <!DOCTYPE html>
+<html lang="en">
+<head>  
+    <title>Document</title>
+</head>
+<body>
+    <h1>Search</h1>
+
+    <form>
+        <input name="q" placeholder="search ..."/>
+        <button>
+            Search
+        </button>
+    </form>
+
+    <h2> Results for: <div id='result'></div></h2>
+
+    <p>No result found.</p>
+</body>
+<script>
+document.getElementById('result').textContent = ${query};
+</script>
+</html>
+        `)
+})
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
 

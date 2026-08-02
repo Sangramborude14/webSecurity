50ca899622fa8de601623308b15c0fde093e62ff
Sangram <sangram2mail@gmail.com>
2026-08-02 11:05:32 +0530

    OWASP: view notes completed

---
+++ OWASP/BAC/frontend/src/app/notes/view/page.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+interface Note {
+  id: string;
+  title: string;
+  content: string;
+}
+
+export default function ViewNote() {
+    
+const [viewnote, setViewnote] = useState<Note[]>([]);
+const [message, setMessage] = useState('');
+
+useEffect(() => {
+const getNotes = async() => {
+   try {
+     const response = await fetch("http://localhost:5000/notes/view", {
+       credentials: 'include',
+     });
+     const notes = await response.json();
+     if (Array.isArray(notes)) {
+       setViewnote(notes);
+     } else if (notes.data && Array.isArray(notes.data)) {
+       setViewnote(notes.data);
+     }
+   } catch(error: any) {
+    setMessage(error.message)
+   }
+}
+getNotes();
+},[])
+
+
+return(<>
+<h1> VIEW NOTES</h1>
+{message && <p>{message}</p>}
+<div>
+    <div>
+        {viewnote.map(note => (
+            <div key={note.id} id={note.id}>
+                <div>
+                    {note.title}
+                </div>
+                <div>
+                <p>{note.content}</p>
+                </div>
+            </div>
+        ))}
+    </div>
+</div>
+</>)
+}
